ef74fb8a1095cfb3446247ccc37499a5ad5c3c49
sava <savamedvedev@gmail.com>
2022-05-22 03:33:19 +0800

    second-commit

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,14 +1,31 @@
-import data from '../mock/data.json';
-import { JobPosition } from './JobPosition';
+import { useSelector, useDispatch } from "react-redux";
+import { JobPosition } from "./JobPosition";
+import { selectVisiblePositions } from "../store/positions/position-selector";
+import { selectFilters } from "../store/filters/filter-selectors";
+import { addFilter } from "../store/filters/filter-actions";
 
 const JobList = () => {
+  const dispatch = useDispatch();
+  const currentFilters = useSelector(selectFilters);
+  const positions = useSelector((state) =>
+    selectVisiblePositions(state, currentFilters)
+  );
+
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
+  };
+
   return (
-    <div className='job-list'>
-      {data.map(item => (
-        <JobPosition key={item.id} {...item} />
+    <div className="job-list">
+      {positions.map((item) => (
+        <JobPosition
+          key={item.id}
+          handleAddFilter={handleAddFilter}
+          {...item}
+        />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export {JobList};
+export { JobList };
