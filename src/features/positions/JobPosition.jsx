c39560770c040cf4d2893d3511ca098edd1ffe5e
sava <savamedvedev@gmail.com>
2022-06-12 23:35:02 +0800

    four-commit

--- src/features/positions/JobPosition.jsx
+++ src/features/positions/JobPosition.jsx
@@ -1,7 +1,7 @@
-import PropTypes from "prop-types";
-import { Badge } from "UI/Badge";
-import { Card } from "UI/Card";
-import { Stack } from "UI/Stack";
+import PropTypes from 'prop-types';
+import { Badge } from 'UI/Badge';
+import { Card } from 'UI/Card';
+import { Stack } from 'UI/Stack';
 
 const JobPosition = ({
   id,
@@ -23,11 +23,15 @@ const JobPosition = ({
 
   return (
     <Card isFeatured={featured}>
-      <div className="job-position">
-        <div className="job-position-info">
-          <img className="job-position-avatar" src={logo} alt={company} />
-          <div className="job-position-body">
-            <div className="job-postion-company">
+      <div className='job-position'>
+        <div className='job-position-info'>
+          <img
+            className='job-position-avatar'
+            src={logo}
+            alt={company}
+          />
+          <div className='job-position-body'>
+            <div className='job-postion-company'>
               <h3>{company}</h3>
               {(isNew || featured) && (
                 <Stack>
@@ -44,27 +48,36 @@ const JobPosition = ({
                 </Stack>
               )}
             </div>
-            <h2 className="job-position-title">{position}</h2>
+            <h2 className='job-position-title'>
+              {position}
+            </h2>
             <Stack>
-              <div className="job-position-meta">{postedAt}</div>
-              <div className="job-position-meta">{contract}</div>
-              <div className="job-position-meta">{location}</div>
+              <div className='job-position-meta'>
+                {postedAt}
+              </div>
+              <div className='job-position-meta'>
+                {contract}
+              </div>
+              <div className='job-position-meta'>
+                {location}
+              </div>
             </Stack>
           </div>
         </div>
         <Stack>
-          {badges.map((item) => (
-            <Badge key={item} onClick={() => handleAddFilter(item)}>
-              {item}
-            </Badge>
+          {badges.map(item => (
+            <Badge
+              key={item}
+              onClick={() => handleAddFilter(item)}
+            >{item}</Badge>
           ))}
         </Stack>
       </div>
     </Card>
-  );
-};
+  )
+}
 
-export { JobPosition };
+export {JobPosition};
 
 JobPosition.propTypes = {
   id: PropTypes.number,
